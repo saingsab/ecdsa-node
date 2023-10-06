878e2eb40e89f627179ab2dbb118aa1d0789b47a
Ayoung <saing.sab@gmail.com>
2023-10-06 22:32:20 +0700

    adding wallet gen

---
+++ server/scripts/generate.js
@@ -0,0 +1,10 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+
+const privateKey = secp.secp256k1.utils.randomPrivateKey();
+console.log(`Private Key: `, toHex(privateKey));
+const publicKey = secp.secp256k1.getPublicKey(privateKey);
+// 20 of keccake256 of hash is use for eth public address
+console.log(`Public Key: `, toHex(publicKey));
+console.log(`Wallet: ${toHex(keccak256(privateKey.slice(1)).slice(-20))}`);
