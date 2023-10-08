1d20dbb3e0da598103758bede4c7dc067c2b9ec2
Ayoung <saing.sab@gmail.com>
2023-10-08 15:25:59 +0700

    upate flow

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "027194955c0df985899e872babd50458b5c5171128200b4e2f5ceeb6f1aa687ff4": 100,
+  "027137a47499b21b95814809fa9d84623ca904734b1c930b9fbd2bcb2e7042c966": 50,
+  "03a57b4e532b51ba73fc12ecdba65ff94622566f273c5a42370892ee0c1f7ac2b4": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +19,17 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+
+    /* TODO
+  send only the signature, get a signature from the client-side application
+
+  recover the valid key from the transfer fund, recover the public address from the signature
+
+  flow -> the sender is the signer
+
+  signature, recipient, amount ->>>
+  */
+
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -5,6 +5,6 @@ const { keccak256 } = require("ethereum-cryptography/keccak");
 const privateKey = secp.secp256k1.utils.randomPrivateKey();
 console.log(`Private Key: `, toHex(privateKey));
 const publicKey = secp.secp256k1.getPublicKey(privateKey);
-// 20 of keccake256 of hash is use for eth public address
 console.log(`Public Key: `, toHex(publicKey));
+// 20 of keccake256 of hash is use for eth public address
 console.log(`Wallet: ${toHex(keccak256(privateKey.slice(1)).slice(-20))}`);
